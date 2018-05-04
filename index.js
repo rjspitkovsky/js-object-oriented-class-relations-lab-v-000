b8b2a7c49cec087980c44fe5403e61dd9f111c10
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-04 21:57:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,22 @@ class Driver {
     })
   }
 }
+
+let passengerId = 0 
+class Passenger {
+  constructor(name) {
+    this.name = name 
+    this.id = ++passengerId
+    store.passengers.push(this)
+  }
+  trips() {
+    store.trips.filter(trip => {
+      return trip.passengerId === this.id 
+    })
+  }
+  drivers() {
+    store.drivers.filter(driver => {
+      return driver.passengerId === this.id
+    })
+  }
+}
