4bb387d9c00fe990953bb0c463f6a2a0b66338bb
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-04 22:18:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,13 +49,13 @@ class Trip {
   }
 
   passenger() {
-    store.passengers.find(passenger => {
+  return  store.passengers.find(passenger => {
       return passenger.id === this.passengerId
     })
   }
 
   driver() {
-    store.drivers.find(driver => {
+  return  store.drivers.find(driver => {
       return driver.id === this.driverId
     })
   }
