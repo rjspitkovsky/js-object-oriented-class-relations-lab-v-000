9614111b8bed1eb8bbb9b33423c6920ffa0dacd4
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-04 22:02:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,21 +20,41 @@ class Driver {
   }
 }
 
-let passengerId = 0 
+let passengerId = 0
 class Passenger {
   constructor(name) {
-    this.name = name 
+    this.name = name
     this.id = ++passengerId
     store.passengers.push(this)
   }
   trips() {
     store.trips.filter(trip => {
-      return trip.passengerId === this.id 
+      return trip.passengerId === this.id
     })
   }
   drivers() {
     store.drivers.filter(driver => {
       return driver.passengerId === this.id
+    })
+  }
+}
+
+let tripId = 0
+class Trip {
+  constructor(driver, passenger) {
+    this.id = ++tripId 
+    this.driverId = driverId(driver) 
+    this.passengerId = passengerId(passenger)
+    store.trips.push(this)
+  }
+  driverId(driver_argument) {
+    store.drivers.find(driver => {
+      return driver.id === driver_argument.id
+    })
+  }
+  passengerId(passenger_argument) {
+    store.passengers.find(passenger => {
+      return passenger.id === passenger_argument.id
     })
   }
 }
