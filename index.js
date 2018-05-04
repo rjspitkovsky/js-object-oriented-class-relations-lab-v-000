21b5b3d04efd3d207d39b3d06114d757ab5d0d1d
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-04 22:17:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,12 +43,8 @@ let tripId = 0
 class Trip {
   constructor(driver, passenger) {
     this.id = ++tripId
-    // if(driver) {
-      this.driverId = driver.id
-    // }
-    // if(passenger) {
-      this.passengerId = passenger.id
-    // }
+    this.driverId = driver.id
+    this.passengerId = passenger.id
     store.trips.push(this)
   }
 
