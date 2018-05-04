549b859f9f92e67fb89105dc19f2cc86c5b8949f
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-04 21:55:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 let store = {drivers: [], passengers: [], trips: []}
 
-let driverId = 0 
+let driverId = 0
 
 class Driver {
   constructor(name) {
-    this.name = name 
+    this.name = name
     this.id = ++driverId
+    store.drivers.push(this)
   }
   trips() {
     store.trips.filter(trip => {
       return trip.driverId === this.id
+    })
+  }
+  passengers() {
+    store.passengers.filter(passenger => {
+      return passenger.driverID === this.id
     })
   }
 }
